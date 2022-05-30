44b5910d286d2a5f3972567536ca75f3c0adb70b
sanchen.wl <sanchen.wl@alibaba-inc.com>
2022-05-30 19:55:46 +0800

    feat: change .gitignore

---
+++ lib/index.d.ts
@@ -0,0 +1,33 @@
+// export * from '@uni/env';
+
+export declare const isWeb: boolean;
+export declare const isNode: boolean;
+export declare const isWeex: boolean;
+export declare const isKraken: boolean;
+export declare const isMiniApp: boolean;
+export declare const isByteDanceMicroApp: boolean;
+export declare const isBaiduSmartProgram: boolean;
+export declare const isKuaiShouMiniProgram: boolean;
+export declare const isWeChatMiniProgram: boolean;
+export declare const isQuickApp: boolean;
+export declare const isPHA: boolean;
+export declare const isFRM: any;
+export declare const isWindVane: boolean;
+export declare const isNativeJS: boolean;
+declare const _default: {
+    isNativeJS: boolean,
+    isWeb: boolean;
+    isNode: boolean;
+    isWeex: boolean;
+    isKraken: boolean;
+    isMiniApp: boolean;
+    isByteDanceMicroApp: boolean;
+    isBaiduSmartProgram: boolean;
+    isKuaiShouMiniProgram: boolean;
+    isWeChatMiniProgram: boolean;
+    isQuickApp: boolean;
+    isPHA: boolean;
+    isWindVane: boolean;
+    isFRM: any;
+};
+export default _default;
